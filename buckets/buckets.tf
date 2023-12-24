f839fd91158e1864d936f1dd1142fc5754e3ccc3
siandji patrick <45380639+siandjiPatrick@users.noreply.github.com>
2023-12-24 18:25:47 +0100

    add a ressource service account

--- buckets/buckets.tf
+++ buckets/buckets.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "bucket_from_terraform" {
   name     = var.bucket_name
   location = var.region
 }
+
+resource "google_service_account" "service_account" {
+  account_id   = var.service_name
+  display_name = var.display_name
+}
