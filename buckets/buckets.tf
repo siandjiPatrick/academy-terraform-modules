b8360769ac622a6e5e6d97f78b5b9bbf46c8d513
siandji patrick <45380639+siandjiPatrick@users.noreply.github.com>
2023-12-24 18:31:47 +0100

    remove service account ressource

--- buckets/buckets.tf
+++ buckets/buckets.tf
@@ -3,7 +3,3 @@ resource "google_storage_bucket" "bucket_from_terraform" {
   location = var.region
 }
 
-resource "google_service_account" "service_account" {
-  account_id   = var.service_name
-  display_name = var.display_name
-}
